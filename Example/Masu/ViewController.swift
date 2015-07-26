c15aaa2d406fe831888967a0be61a1182f6a16f3
midnightSuyama <midnightSuyama@gmail.com>
2015-07-27 01:13:25 +0900

    Initial commit

---
+++ Example/Masu/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Masu
+//
+//  Created by midnightSuyama on 07/27/2015.
+//  Copyright (c) 2015 midnightSuyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
